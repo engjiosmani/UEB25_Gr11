1a2b85cb33472f79e6c5ad61945c1711b62a1375
edonitagashi <161738953+edonitagashi@users.noreply.github.com>
2025-04-13 23:27:50 +0200

    Getters dhe Setters
    Perdorimi i get dhe set te klasa User

--- klasat/User.php
+++ klasat/User.php
@@ -20,5 +20,38 @@ class User {
         return $email === $this->email && $password === $this->password;
     }
     
+    // Getters
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getDob() {
+        return $this->dob;
+    }
+
+    // Setters
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function setDob($dob) {
+        $this->dob = $dob;
+    }
 }
 ?>
