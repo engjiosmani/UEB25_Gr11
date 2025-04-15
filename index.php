93eae4807e89fdc8f8c30a72e340c8e1a7a359ea
fatlindaosdautaj <fatlindaosdautaj0@gmail.com>
2025-04-15 21:18:48 +0200

    Multidimensional Arrays

--- index.php
+++ index.php
@@ -312,100 +312,112 @@ if (isset($_GET['sort'])) {
 
 
         <section class="schedule-section section-padding" id="section_4">
-            <div class="container">
-                <div class="row">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="text-white mb-4">Event Schedule</h2>
 
-                    <div class="col-12 text-center">
-                        <h2 class="text-white mb-4">Event Schedule</h1>
+                <?php
+                // 1. Definimi i të dhënave të orarit si multidimensional array
+                $schedule = [
+                    'Day 1' => [
+                        'Wednesday' => [
+                            'title' => 'Pop Night',
+                            'time' => '5:00 - 7:00 PM',
+                            'artist' => 'Adele',
+                            'background' => 'pop-background-image'
+                        ],
+                        'Thursday' => [
+                            'background' => '#F3DCD4'
+                        ],
+                        'Friday' => [
+                            'title' => 'Rock & Roll',
+                            'time' => '7:00 - 11:00 PM',
+                            'artist' => 'Rihana',
+                            'background' => 'rock-background-image'
+                        ]
+                    ],
+                    'Day 2' => [
+                        'Wednesday' => [
+                            'background' => '#ECC9C7'
+                        ],
+                        'Thursday' => [
+                            'title' => 'DJ Night',
+                            'time' => '6:30 - 9:30 PM',
+                            'artist' => 'Rihana'
+                        ],
+                        'Friday' => [
+                            'background' => '#D9E3DA'
+                        ]
+                    ],
+                    'Day 3' => [
+                        'Wednesday' => [
+                            'title' => 'Country Music',
+                            'time' => '4:30 - 7:30 PM',
+                            'artist' => 'Rihana',
+                            'background' => 'country-background-image'
+                        ],
+                        'Thursday' => [
+                            'background' => '#D1CFC0'
+                        ],
+                        'Friday' => [
+                            'title' => 'Free Styles',
+                            'time' => '6:00 - 10:00 PM',
+                            'artist' => 'By Members'
+                        ]
+                    ]
+                ];
+                ?>
 
-                            <div class="table-responsive">
-                                <table class="schedule-table table table-dark">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Date</th>
-
-                                            <th scope="col">Wednesday</th>
-
-                                            <th scope="col">Thursday</th>
-
-                                            <th scope="col">Friday</th>
-
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">Day 1</th>
-
-                                            <td class="table-background-image-wrap pop-background-image">
-                                                <h3>Pop Night</h3>
-
-                                                <p class="mb-2">5:00 - 7:00 PM</p>
-
-                                                <p>By Adele</p>
-
-                                                <div class="section-overlay"></div>
-                                            </td>
-
-                                            <td style="background-color: #F3DCD4"></td>
-
-                                            <td class="table-background-image-wrap rock-background-image">
-                                                <h3>Rock & Roll</h3>
-
-                                                <p class="mb-2">7:00 - 11:00 PM</p>
-
-                                                <p>By Rihana</p>
-
-                                                <div class="section-overlay"></div>
-                                            </td>
-                                        </tr>
-
-                                        <tr>
-                                            <th scope="row">Day 2</th>
-
-                                            <td style="background-color: #ECC9C7"></td>
-
-                                            <td>
-                                                <h3>DJ Night</h3>
-
-                                                <p class="mb-2">6:30 - 9:30 PM</p>
-
-                                                <p>By Rihana</p>
-                                            </td>
-
-                                            <td style="background-color: #D9E3DA"></td>
-                                        </tr>
-
-                                        <tr>
-                                            <th scope="row">Day 3</th>
-
-                                            <td class="table-background-image-wrap country-background-image">
-                                                <h3>Country Music</h3>
-
-                                                <p class="mb-2">4:30 - 7:30 PM</p>
-
-                                                <p>By Rihana</p>
-
-                                                <div class="section-overlay"></div>
-                                            </td>
-
-                                            <td style="background-color: #D1CFC0"></td>
-
-                                            <td>
-                                                <h3>Free Styles</h3>
-
-                                                <p class="mb-2">6:00 - 10:00 PM</p>
-
-                                                <p>By Members</p>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                    </div>
+                <!-- 2. Tabela dinamike -->
+                <div class="table-responsive">
+                    <table class="schedule-table table table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">Date</th>
+                                <th scope="col">Wednesday</th>
+                                <th scope="col">Thursday</th>
+                                <th scope="col">Friday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($schedule as $day => $events): ?>
+                                <tr>
+                                    <th scope="row"><?= $day ?></th>
+                                    <?php 
+                                    // Renditja e ditëve sipas renditjes së dëshiruar
+                                    $daysOrder = ['Wednesday', 'Thursday', 'Friday'];
+                                    foreach ($daysOrder as $dayName): 
+                                        $event = $events[$dayName] ?? [];
+                                    ?>
+                                        <td
+                                      <?php if (isset($event['background'])): ?>
+                                      <?php if (strpos($event['background'], '-background-image') !== false): ?>
+                                     class="table-background-image-wrap <?= $event['background'] ?>"
+                                     <?php else: ?>
+                                     style="background-color: <?= $event['background'] ?>"
+                                      <?php endif; ?>
+                                       <?php endif; ?>
+                                     >
+                                      <?php if (!empty($event['title'])): ?>
+                                      <h3><?= $event['title'] ?></h3>
+                                       <p class="mb-2"><?= $event['time'] ?></p>
+                                       <p>By <?= $event['artist'] ?></p>
+                                        <?php if (strpos($event['background'] ?? '', '-background-image') !== false): ?>
+                                        <div class="section-overlay"></div> 
+                                        <?php endif; ?>
+                                        <?php endif; ?>
+                                     </td>
+                                    <?php endforeach; ?>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+</section>
 
 
         <section class="pricing-section section-padding section-bg" id="section_5">
