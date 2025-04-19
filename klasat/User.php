b4573687b49898db742099879b9869445cd14e4f
edonitagashi <161738953+edonitagashi@users.noreply.github.com>
2025-04-19 16:38:45 +0200

    aksesueshmeria dhe getters and setters

--- klasat/User.php
+++ klasat/User.php
@@ -1,8 +1,8 @@
 <?php
 class User {
     public $fullname;
-    public $email;
-    public $password;
+    private $email;
+    private $password;
     public $birthday;
 
     public function __construct($fullname, $email, $password, $birthday) {
@@ -12,15 +12,31 @@ class User {
         $this->birthday = $birthday;
     }
 
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
     public function displayInfo() {
         echo "Përdoruesi u regjistrua me sukses!<br>";
         echo "Emri i plotë: $this->fullname <br>";
-        echo "Email: $this->email <br>";
+        echo "Email:" . $this->getEmail() ." <br>";
         echo "Data e lindjes: $this->birthday <br>";
     }
 
     public function login($email, $password) {
-        if ($this->email === $email && $this->password === $password) {
+        if ($this->getEmail() === $email && $this->getPassword() === $password) {
             echo "Kyçja me sukses! Mirë se vini, $this->fullname";
         } else {
             echo "Email ose fjalëkalim i gabuar.";
