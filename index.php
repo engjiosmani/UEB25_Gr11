8a46239e1f05099e677d2955a60b477c7de07d65
edonitagashi <edonita.gashi@student.uni-pr.edu>
2025-05-26 18:04:55 +0200

    Largimi i references me unset()

--- index.php
+++ index.php
@@ -425,6 +425,8 @@ if (isset($_SESSION['ticket_error'])) {
             $firstArtist = &$artists['Madona'];
             $firstArtist['age'] = 70;
 
+            unset($firstArtist);
+
             $sort_type = $_GET['sort'] ?? 'name_asc';
 
             switch ($sort_type) {
